61135b39fbca1fff79ca0574b65947f98f0790c3
Khuslensaikhan Battsetseg <98379236+Huska69@users.noreply.github.com>
2023-01-16 23:40:21 +0800

    Lab of programming 1, week 2.4

--- Lab of programming 1, week 2.4.c
+++ Lab of programming 1, week 2.4.c
@@ -1,6 +1,6 @@
 //NDHU CSIE Lab of programming 1, week 2.4
 
-//Hashmat is a brave warrior who with his group of young soldiers moves from one place to another tofight against his opponents. 
+//Hashmat is a brave warrior who with his group of young soldiers moves from one place to another tofight against his opponents
 //Before Fighting he just calculates one thing, the difference between hissoldier number and the opponent’s soldier number. 
 //From this difference he decides whether to fight ornot. Hashmat’s soldier number is never greater than his opponent.
 
