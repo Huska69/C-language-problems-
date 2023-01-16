361495e5d4aaabd43eebb26d6f2c8c67bdab8137
Khuslensaikhan Battsetseg <98379236+Huska69@users.noreply.github.com>
2023-01-16 23:40:06 +0800

    Lab of programming 1, week 2.3

--- Lab of programming 1, week 2.3.c
+++ Lab of programming 1, week 2.3.c
@@ -3,7 +3,7 @@
 //An arithmetic progression or arithmetic sequence (AP) is a sequence of numbers such that the difference between the consecutive terms is constant. For instance, the sequence 5, 7, 9, 11, 13, 15, . . . is an arithmetic progression with a common difference of 2.
 //If the initial term of an arithmetic progression isaand the common difference of successive members isd, then then-th term of the sequence ({ \displaystyle  a_{n} }) is given by:
 //{\displaystyle a_{n}=a+(n-1)d},
-//Write a program to read in the initial term, common difference and a number N, then display the N-th term.
+//Write a program to read in the initial term, common difference and a number N, then display the N-th term
 
 #include <stdio.h>
 #include <stdlib.h>
