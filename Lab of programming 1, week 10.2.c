44106e443e069209bca079a02b9c97b996b89868
Khuslensaikhan Battsetseg <98379236+Huska69@users.noreply.github.com>
2023-01-29 00:56:09 +0800

    Lab of programming 1, week 10.2

---
+++ Lab of programming 1, week 10.2.c
@@ -0,0 +1,39 @@
+//NDHU CSIE Lab of programming 1, week 10.2
+//Declare a function call isDigit, function isDigit get one parameter which is a char, return 1 if the parameter is '0', '1', '2', '3', '4', '5', '6', '7', '8' or '9', return -1 in other case.
+
+int  isDigit(char input){
+if (input == '0'){
+    return 1;
+}
+if (input == '1'){
+    return 1;
+}
+if (input == '2'){
+    return 1;
+}
+if (input == '3'){
+    return 1;
+}
+if (input == '4'){
+    return 1;
+}
+if (input == '5'){
+    return 1;
+}
+if (input == '6'){
+    return 1;
+}
+if (input == '7'){
+    return 1;
+}
+if (input == '8'){
+    return 1;
+}
+if (input == '9'){
+    return 1;
+}
+else {
+    return -1;
+}
+};
+
